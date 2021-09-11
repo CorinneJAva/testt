d0e2d52ee9f567380d75989e2f8eb9399d17d7f6
CorinneJAva <corinnecelanie@gmail.com>
2021-09-11 11:22:06 +0200

    first commit2

--- src/main/java/com/example/heruko/demo/controller/WelcomeController.java
+++ src/main/java/com/example/heruko/demo/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 	
-	@GetMapping("/WELCOME")
+	@GetMapping("/")
 	public String welcome() {
 	return "Welcome to spring bot heruku demo";
 }
